1ff51b69c1f0389e1a44778d79b1dd71c2af27f5
svaditya <123.aditya@gmail.com>
2016-12-19 10:55:07 -0500

    styles

---
+++ javascripts/blog_footer.js
@@ -0,0 +1,16 @@
+document.write('\
+    <!-- FOOTER  -->\
+    <div id="footer_wrap" class="outer">\
+      <footer class="inner">\
+        <section class="clearfix">\
+          <a id="project_tagline" href="blog.html"><span>Data Science and Robots Blog</span></a>\
+          <p>\
+            <br>\
+            <a href="https://creativecommons.org/publicdomain/zero/1.0/">\
+              <img title="Creative Commons Zero license" src="images/cc0.png" alt="CC0" style="width: 90px;" />\
+            </a> &nbsp except where noted.\
+          </p>\
+        </section>\
+      </footer>\
+    </div>\
+');
